e40235841759959db6e95388841a4dddaebb0d00
Maksim <kozyrev.maksim.km@gmail.com>
2025-07-08 18:16:12 +0300

    New

---
+++ python-apps/users/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/test')
+def test_endpoint():
+    return [os.getenv("MONGO_TASKS_URI"), os.getenv("MONGO_USERS_URI")]
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8001)
