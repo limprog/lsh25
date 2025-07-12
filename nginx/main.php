c49dd56186093b27e769182c1fe89f32a55d45b2
Maksim <kozyrev.maksim.km@gmail.com>
2025-07-12 17:04:32 +0300

    New

--- nginx/main.php
+++ nginx/main.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+if (isset($_SESSION["bearerToken"])){
+  $session = true;
+  $bearerToken = $_SESSION["bearerToken"];
+  echo "Сессия активна. $bearerToken";
+} else {
+  $session = false;
+  echo "Сессия неактивна.";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
