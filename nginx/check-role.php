0f810f70d6da2170d64072bd2920e5038ab52d2d
Maksim <kozyrev.maksim.km@gmail.com>
2025-07-13 22:45:54 +0300

    New

--- nginx/check-role.php
+++ nginx/check-role.php
@@ -1,17 +1,21 @@
-  if (isset($_COOKIE["role"])){
-    $userRole = $_COOKIE["role"];
-  } else {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://89.169.146.136:8081/users/role?username=" . $userLogin);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization: Bearer " . $bearerToken
-    ]);
-    $result = curl_exec($ch);
+<?php
 
-    if (curl_error($ch)){
-      $userRole = false;
-    } else {
-      $userRole = $result;
-    }
+if (isset($_COOKIE["role"])){
+  $userRole = $_COOKIE["role"];
+} else {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "http://89.169.146.136:8081/users/role?username=" . $userLogin);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      "Authorization: Bearer " . $bearerToken
+  ]);
+  $result = curl_exec($ch);
+
+  if (curl_error($ch)){
+    $userRole = false;
+  } else {
+    $userRole = $result;
   }
+}
+
+?>
